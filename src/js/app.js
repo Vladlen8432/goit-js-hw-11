6b4c43ac5157b82029ce6d37446a2614a6df5607
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-09-22 22:10:23 +0300

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,94 +1,84 @@
-import { getPhotos } from './pixabay-api';
-import SimpleLightbox from 'simplelightbox';
+import { getPhotos } from "./pixabay-api";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const formEl = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
+const formEl = document.querySelector("#search-form");
+const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more");
+
 let page = 1;
 let totalHits = 0;
 let lightbox = null;
 
-formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener("submit", onSubmit);
 
 async function onSubmit(e) {
   e.preventDefault();
-  gallery.innerHTML = '';
+  gallery.innerHTML = "";
   page = 1;
-
   const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
 
   if (!searchQuery) {
-    return alert('Empty query');
+    return alert("Empty query");
   }
-
   try {
     const res = await getPhotos(page, searchQuery);
     totalHits = res.totalHits;
     renderImages(res.hits);
-
     if (page * 40 < totalHits) {
-      loadMoreBtn.style.display = 'block';
+      loadMoreBtn.style.display = "block";
     } else {
-      loadMoreBtn.style.display = 'none';
+      loadMoreBtn.style.display = "none";
     }
-
     console.log(res);
   } catch (error) {
     console.error(error);
   }
 }
-
 function renderImages(images) {
   const fragmentImg = document.createDocumentFragment();
-
   images.forEach(image => {
-    const li = document.createElement('li');
-    li.classList.add('gallery-card');
-
-    li.innerHTML = `<a htef="#"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-      <p class="info-item"><b>Views:</b> ${image.views}</p>
-      <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-      <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-    </div></a>`;
-
+    const li = document.createElement("li");
+    li.classList.add("gallery-card");
+    li.innerHTML = `<a href="${image.largeImageURL}">
+      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    </a>`;
     fragmentImg.appendChild(li);
   });
-
   gallery.appendChild(fragmentImg);
-
   if (lightbox) {
     lightbox.refresh();
   } else {
-    lightbox = new SimpleLightbox('.gallery-card img', {
-      captionsData: 'alt',
+    lightbox = new SimpleLightbox(".gallery-card a", {
+      captionsData: "alt",
       captionDelay: 250,
       captions: true,
       animationSpeed: 250,
     });
   }
-
-  gallery.querySelectorAll('.gallery-card img').forEach(img => {
-    img.addEventListener('click', () => {
-      lightbox.open(img);
+  gallery.querySelectorAll(".gallery-card a").forEach(a => {
+    a.addEventListener("click", e => {
+      e.preventDefault();
+      lightbox.open(a);
     });
   });
 }
-
 const onLoadMore = () => {
   page++;
-
   const searchQuery = document
     .querySelector('input[name="searchQuery"]')
     .value.trim();
-
   getPhotos(page, searchQuery)
     .then(res => {
       renderImages(res.hits);
-
       if (page * 40 >= res.totalHits) {
-        loadMoreBtn.style.display = 'none';
+        loadMoreBtn.style.display = "none";
         alert("We're sorry, but you've reached the end of search results.");
       }
     })
@@ -96,221 +86,4 @@ const onLoadMore = () => {
       console.error(error);
     });
 };
-
-loadMoreBtn.addEventListener('click', onLoadMore);
-
-
-// import { getPhotos } from './pixabay-api';
-// import SimpleLightbox from 'simplelightbox';
-
-// const formEl = document.querySelector('#search-form');
-// const gallery = document.querySelector('.gallery');
-// const loadMoreBtn = document.querySelector('.load-more');
-// let page = null;
-// let lightbox = null;
-
-// formEl.addEventListener('submit', onSubmit);
-
-// async function onSubmit(e) {
-//   e.preventDefault();
-//   gallery.innerHTML = '';
-//   page = 1;
-
-//   const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-
-//   if (!searchQuery) {
-//     return alert('Empty query');
-//   }
-
-//   try {
-//     const res = await getPhotos(page, searchQuery);
-//     renderImages(res.hits);
-
-//     if (page * 40 < res.totalHits) {
-//       loadMoreBtn.style.display = 'block';
-//     } else {
-//       loadMoreBtn.style.display = 'none';
-//     }
-
-//     console.log(res);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function renderImages(images) {
-//   const fragmentImg = document.createDocumentFragment();
-
-//   images.forEach(image => {
-//     const li = document.createElement('li');
-//     li.classList.add('gallery-card');
-
-//     li.innerHTML = `<a>
-//     <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-//         <div class="info">
-//           <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-//           <p class="info-item"><b>Views:</b> ${image.views}</p>
-//           <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-//           <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-//         </div>
-//     </a>`;
-
-//     fragmentImg.appendChild(li);
-//   });
-
-//   gallery.appendChild(fragmentImg);
-
-//   if (lightbox) {
-//     lightbox.refresh();
-//   } else {
-//     lightbox = new SimpleLightbox('.gallery-card a');
-//   }
-
-//   gallery.querySelectorAll('.gallery-card img').forEach(img => {
-//     img.addEventListener('click', () => {
-//       lightbox.open(img);
-//     });
-//   });
-// }
-
-// const onLoadMore = () => {
-//   page++;
-
-//   const searchQuery = document
-//     .querySelector('input[name="searchQuery"]')
-//     .value.trim();
-
-//   getPhotos(page, searchQuery)
-//     .then(res => {
-//       renderImages(res.hits);
-
-//       if (page * 40 >= res.totalHits) {
-//         loadMoreBtn.style.display = 'none';
-//         alert("We're sorry, but you've reached the end of search results.");
-//       }
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// };
-
-// loadMoreBtn.addEventListener('click', onLoadMore);
-
-
-
-
-// import { getPhotos } from './pixabay-api';
-// import SimpleLightbox from 'simplelightbox';
-
-// const formEl = document.querySelector('#search-form');
-// const gallery = document.querySelector('.gallery');
-// const loadMoreBtn = document.querySelector('.load-more');
-// let page = 1;
-// let totalHits = 0;
-// let lightbox = null;
-
-// formEl.addEventListener('submit', onSubmit);
-
-// async function onSubmit(e) {
-//   e.preventDefault();
-//   gallery.innerHTML = '';
-//   page = 1;
-
-//   const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-
-//   if (!searchQuery) {
-//     return alert('Empty query');
-//   }
-
-//   try {
-//     const res = await getPhotos(page, searchQuery);
-//     totalHits = res.totalHits;
-//     renderImages(res.hits);
-
-//     if (page * 40 < totalHits) {
-//       loadMoreBtn.style.display = 'block';
-//     } else {
-//       loadMoreBtn.style.display = 'none';
-//     }
-
-//     console.log(res);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function renderImages(images) {
-//   const fragmentImg = document.createDocumentFragment();
-
-//   images.forEach(image => {
-//     const li = document.createElement('li');
-//     li.classList.add('gallery-card');
-
-//     li.innerHTML = `<a>
-//     <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-//         <div class="info">
-//           <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-//           <p class="info-item"><b>Views:</b> ${image.views}</p>
-//           <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-//           <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-//         </div>
-//     </a>`;
-
-//     fragmentImg.appendChild(li);
-//   });
-
-//   gallery.appendChild(fragmentImg);
-
-//   if (lightbox) {
-//     lightbox.refresh();
-//   } else {
-//     lightbox = new SimpleLightbox('.gallery-card a');
-//   }
-
-//   gallery.querySelectorAll('.gallery-card img').forEach(img => {
-//     img.addEventListener('click', () => {
-//       lightbox.open(img);
-//     });
-//   });
-// }
-
-// const onLoadMore = () => {
-//   page++;
-
-//   const searchQuery = document
-//     .querySelector('input[name="searchQuery"]')
-//     .value.trim();
-
-//   getPhotos(page, searchQuery)
-//     .then(res => {
-//       renderImages(res.hits);
-
-//       if (page * 40 >= res.totalHits) {
-//         loadMoreBtn.style.display = 'none';
-//         alert("We're sorry, but you've reached the end of search results.");
-//       }
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// };
-
-// loadMoreBtn.addEventListener('click', onLoadMore);
-
-// const renderImage = images => {
-//   const markup = images.map(
-//     image => `<li>
-//     <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-//       <div class="info">
-//         <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-//         <p class="info-item"><b>Views:</b> ${image.views}</p>
-//         <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-//         <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-//       </div>
-//     </li>`
-//   ).join('');
-
-//   list.innerHTML = markup
-// };
-
-// renderImage()
+loadMoreBtn.addEventListener("click", onLoadMore);
